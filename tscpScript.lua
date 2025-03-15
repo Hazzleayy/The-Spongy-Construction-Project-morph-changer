31c9b5a64de1ff6a3ae7183c4d17acdec90fa1e6
Hazzle <119156502+Hazzleayy@users.noreply.github.com>
2025-03-15 12:15:26 +0100

    Update tscpScript.lua

--- tscpScript.lua
+++ tscpScript.lua
@@ -1,39 +1,39 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
     Name = "TSCP Hub",
-    Icon = 7179075352, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+    Icon = 7179075352, 
     LoadingTitle = "The Spongy Construction Project Tools",
     LoadingSubtitle = "by Systemps",
-    Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+    Theme = "Default", 
  
     DisableRayfieldPrompts = false,
-    DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+    DisableBuildWarnings = false, 
  
     ConfigurationSaving = {
        Enabled = false,
-       FolderName = nil, -- Create a custom folder for your hub/game
+       FolderName = nil, 
        FileName = "TSCP"
     },
  
     Discord = {
-       Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
-       Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
-       RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+       Enabled = false, 
+       Invite = "noinvitelink", 
+       RememberJoins = true 
     },
  
-    KeySystem = false, -- Set this to true to use our key system
+    KeySystem = false, 
     KeySettings = {
        Title = "Untitled",
        Subtitle = "Key System",
-       Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
-       FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-       Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+       Note = "No method of obtaining the key is provided", 
+       FileName = "Key", 
+       SaveKey = true, 
+       GrabKeyFromSite = false, 
+       Key = {"Hello"}
     }
  })
 
-local Tab = Window:CreateTab("Morphs", 7179075352) -- Title, Image
+local Tab = Window:CreateTab("Morphs", 7179075352) 
 local Section = Tab:CreateSection("Basic morphs")
 local Button = Tab:CreateButton({
    Name = "Spongebob",
